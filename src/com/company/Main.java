f957b803c6281e6100e7aa5c999dffa6cc0e684a
Denis Urakanov <mcquicklyss@gmail.com>
2019-04-22 17:59:22 +0500

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Main {
@@ -21,7 +22,7 @@ public class Main {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                panel.move(4,1);
+                panel.move(panel.getxDirection(),panel.getyDirection());
             }
         }
 
@@ -29,11 +30,39 @@ public class Main {
         ActionListener listener = new TimeListener();
         Timer timer = new Timer(50,listener);
         timer.start();
+
+        myFrame.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                panel.doReverse();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
     }
 }
 
-class LabFrame extends JFrame {
-  LabFrame(){
+class LabFrame extends JFrame{
+
+    LabFrame(){
         super("Лабораторная работа №3");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500,400);
@@ -43,10 +72,21 @@ class LabFrame extends JFrame {
 
 class MyPanel extends JComponent{
     private Rectangle rectangle;
+    private int xDirection = 4;
+    private int yDirection = 1;
 
     public MyPanel(){
         rectangle = new Rectangle(10,10,80,50);
     }
+
+    public int getxDirection() {
+        return xDirection;
+    }
+
+    public int getyDirection() {
+        return yDirection;
+    }
+
     public void paint(Graphics g){
         Graphics2D graphics2D = (Graphics2D) g;
         graphics2D.draw(rectangle);
@@ -55,5 +95,14 @@ class MyPanel extends JComponent{
     public void move(int x,int y){
         rectangle.translate(x,y);
         repaint();
+    }
+    public void doReverse(){
+        if (xDirection == 4 && yDirection == 1){
+            xDirection = -4;
+            yDirection = -1;
+        }else{
+            xDirection = 4;
+            yDirection = 1;
+        }
     }
 }
